4d6273d34f00cd6dc653b7d48d3e22d8d501628d
NANDO7U7 <co60134245@gmail.com>
2024-02-11 23:13:04 -0600

    si

--- jv.js
+++ jv.js
@@ -1,20 +1,49 @@
-var btn = document.getElementById("btn"),
-caja = document.getElementById("caja")
-contador=0;
+//elementos click
+var btn = document.getElementById("btn");
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
 
-function cambio()
-{
-    if(contador==0)
-    {
-        caja.classList.add('azul');
-        contador=1;
-    }
-     else{
-        caja.classList.remove('azul');
-        contador=0;
-     }
-    
+var caja = document.getElementById("caja");
+var caja1 = document.getElementById("caja1");
+var caja2 = document.getElementById("caja2");
+
+function cambiarColor(caja, color) {
+  caja.style.backgroundColor = color;
 }
 
+btn.addEventListener("click", function() {
+  cambiarColor(caja, "green");
+});
 
-btn.addEventListener("click",cambio,true)
+btn1.addEventListener("click", function() {
+  cambiarColor(caja1, "blue");
+});
+
+btn2.addEventListener("click", function() {
+    cambiarColor(caja2, "yellow");
+  });
+
+  //elementos mauseover
+var elemento1 = document.getElementById("elemento1");
+var elemento2 = document.getElementById("elemento2");
+var elemento3 = document.getElementById("elemento3");
+
+function aplicarEstilo(elemento) {
+  elemento.classList.add("estiloClasico");
+}
+
+elemento1.addEventListener("mouseover", function() {
+  aplicarEstilo(elemento1);
+});
+
+elemento2.addEventListener("mouseover", function() {
+  aplicarEstilo(elemento2);
+});
+
+elemento3.addEventListener("mouseover", function() {
+  aplicarEstilo(elemento3);
+});
+
+
+
+
